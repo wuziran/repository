912789abac9b75d3d91c370e0545e116f459fab7
Administrator <Administrator@PC-20171007QYVZ>
2019-04-22 16:15:31 +0800

    这是第一次提交

---
+++ springBoot_day04/src/main/java/com/guigu/git/SpringBootDay04Application.java
@@ -0,0 +1,13 @@
+package com.guigu.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDay04Application.class, args);
+	}
+
+}
